eec328ab696c553924be22166c1f81da12419322
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-06-03 14:30:05 +0100

    inits code base

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # reshifter
 A cluster admin backup and restore tool for OpenShift
+
+
+
+## Development
+
+Using OpenShift:
+
+```
+git clone https://github.com/mhausenblas/reshifter.git
+
+oc new-project reshifter
+oc new-build --strategy=docker --name='rs-app' --context-dir='./app/'
+oc start-build rs-app --from-dir .
+oc logs -f bc/rs-app
+
+oc run reshifter --image=$REGISTRY_IP:5000/reshifter/rs-app
+oc expose dc reshifter --port=8080
+oc expose svc/reshifter
+http http://rs-app-reshifter.example.com/v1/backup
+```
---
+++ app/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// BurryConfig holds all relevant config parameters for burry to run
+type BurryConfig struct {
+	Target   string `json:"target"`
+	Endpoint string `json:"endpoint"`
+}
+
+func main() {
+	go ui()
+	go api()
+	select {}
+}
+
+func api() {
+	bc := BurryConfig{
+		Target:   "local",
+		Endpoint: "",
+	}
+	http.HandleFunc("/v1/backup", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(bc)
+	})
+	log.Println("Serving API")
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func ui() {
+	fs := http.FileServer(http.Dir("ui"))
+	http.Handle("/", fs)
+	log.Println("Serving UI")
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func backup() {
+	var url = ""
+	var accessToken = ""
+	var bearer = fmt.Sprintf("Bearer %s", accessToken)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("authorization", bearer)
+
+	// TBD: fetch content
+
+	// resp, err := client.Do(req)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp.Body.Close()
+	//
+	// body, _ := ioutil.ReadAll(resp.Body)
+}
