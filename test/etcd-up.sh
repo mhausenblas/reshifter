f8eecca9b772eacef2153ad60ff83e5ce424e789
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-06-07 06:07:33 +0100

    updates etcd test setup

--- test/etcd-up.sh
+++ test/etcd-up.sh
@@ -7,5 +7,5 @@ set -o pipefail
 
 docker run --rm -d -p 2379:2379 --name test-etcd3 quay.io/coreos/etcd:v3.1.0 /usr/local/bin/etcd --advertise-client-urls http://0.0.0.0:2379 --listen-client-urls http://0.0.0.0:2379
 
-curl localhost:2379/v2/keys/foo -XPUT -d value="bar"
+curl localhost:2379/v2/keys/foo -XPUT -d value="some"
 curl localhost:2379/v2/keys/that/here -XPUT -d value="moar"
