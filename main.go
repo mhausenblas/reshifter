ee68e0e7ff71d72a4428d66bd336e6120e0ebb6d
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-06-17 10:02:47 +0100

    inits Prometheus instrumentation

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/mhausenblas/reshifter/pkg"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // BurryConfig holds all relevant config parameters for burry to run
@@ -22,21 +24,35 @@ func main() {
 }
 
 func api() {
+	backupTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "dev",
+			Subsystem: "app_server",
+			Name:      "backup_total",
+			Help:      "The count of backup attempts.",
+		},
+		[]string{"outcome"},
+	)
 	bc := BurryConfig{
 		Target:   "local",
 		Endpoint: "localhost:2379",
 	}
+	prometheus.MustRegister(backupTotal)
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/v1/version", func(w http.ResponseWriter, r *http.Request) {
 		version := "0.1"
 		fmt.Fprintf(w, "ReShifter in version %s", version)
 	})
 	http.HandleFunc("/v1/backup", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		outcome := "success"
 		err := etcd.Backup(bc.Endpoint)
 		if err != nil {
+			outcome = "failed"
 			log.Error(err)
 		}
 		_ = json.NewEncoder(w).Encode(bc)
+		backupTotal.WithLabelValues(outcome).Inc()
 	})
 	log.Println("Serving API from /v1")
 	_ = http.ListenAndServe(":8080", nil)
