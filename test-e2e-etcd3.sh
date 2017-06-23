34f11d14ccfc8374f6953a1b1fdf5fcde433b6b6
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-06-23 12:19:43 +0100

    inits etcd3 end-to-end testing

--- test-e2e-etcd3.sh
+++ test-e2e-etcd3.sh
@@ -10,8 +10,8 @@ type http >/dev/null 2>&1 || { echo >&2 "Need http command but it's not installe
 type jq >/dev/null 2>&1 || { echo >&2 "Need jq command but it's not installed. You can get it from https://stedolan.github.io/jq/"; exit 1; }
 
 
-etcd2up () {
-  dr=$(docker run --rm -d -p 2379:2379 --name test-etcd --dns 8.8.8.8 quay.io/coreos/etcd:v2.3.8 --advertise-client-urls http://0.0.0.0:2379 --listen-client-urls http://0.0.0.0:2379)
+etcd3up () {
+  dr=$(docker run --rm -d -p 2379:2379 --name test-etcd --dns 8.8.8.8 quay.io/coreos/etcd:v3.1.0 /usr/local/bin/etcd --advertise-client-urls http://0.0.0.0:2379 --listen-client-urls http://0.0.0.0:2379)
   sleep 3s
 }
 
@@ -21,8 +21,8 @@ etcddown () {
 
 
 printf "\n=========================================================================\n"
-printf "Ramping up etcd2 and populating it with a few keys:\n"
-etcd2up
+printf "Ramping up etcd3 and populating it with a few keys:\n"
+etcd3up
 curl localhost:2379/v2/keys/foo -XPUT -d value="bar"
 curl localhost:2379/v2/keys/that/here -XPUT -d value="moar"
 curl localhost:2379/v2/keys/this:also -XPUT -d value="escaped"
@@ -43,16 +43,16 @@ bid=$(http localhost:8080/v1/backup | jq -r .backupid)
 printf "got backup ID %s\n" $bid
 
 printf "\n=========================================================================\n"
-printf "Restarting etcd2 ...\n"
+printf "Restarting etcd3 ...\n"
 etcddown
-etcd2up
+etcd3up
 
 printf "\n=========================================================================\n"
 printf "Performing restore operation:\n"
 http localhost:8080/v1/restore?archive=$bid
 
 printf "=========================================================================\n"
-printf "Cleaning up:\nremoving local backup file\ntearing down etcd2\n"
+printf "Cleaning up:\nremoving local backup file\ntearing down etcd3\n"
 rm ./$bid.zip
 kr=$(kill $RESHIFTER_PID)
 etcddown
