33551b0a12c914309b4732e604eafec7a7f47672
Michael Hausenblas <michael.hausenblas@gmail.com>
2017-06-23 06:04:11 +0100

    improves discovery tests

--- pkg/discovery/discovery_test.go
+++ pkg/discovery/discovery_test.go
@@ -9,51 +9,47 @@ import (
 
 var (
 	probetests = []struct {
-		scheme  string
-		port    string
-		version string
-		secure  bool
+		launchfunc func(string) error
+		scheme     string
+		port       string
+		version    string
+		secure     bool
 	}{
-		{"http", "2379", "2", false},
-		{"http", "2379", "3", false},
+		{util.Etcd2up, "http", "2379", "2", false},
+		{util.Etcd3up, "http", "2379", "3", false},
 	}
 )
 
 func TestProbeEtcd(t *testing.T) {
+	for _, tt := range probetests {
+		testEtcdX(t, tt.launchfunc, tt.scheme, tt.port, tt.version, tt.secure)
+	}
+	_, _, err := ProbeEtcd("localhost")
+	if err == nil {
+		t.Error(err)
+	}
+	_, _, err = ProbeEtcd("localhost:2379")
+	if err == nil {
+		t.Error(err)
+	}
+}
+
+func testEtcdX(t *testing.T, etcdLaunchFunc func(string) error, scheme string, port string, version string, secure bool) {
 	defer func() {
 		_ = util.Etcddown()
 	}()
-	// etcd2 discovery:
-	p := probetests[0].port
-	tetcd := "localhost:" + p
-	err := util.Etcd2up(p)
+	tetcd := "localhost:" + port
+	err := etcdLaunchFunc(port)
 	if err != nil {
 		t.Errorf("Can't launch etcd at %s: %s", tetcd, err)
 		return
 	}
-	v, s, err := ProbeEtcd(probetests[0].scheme + "://" + tetcd)
+	v, s, err := ProbeEtcd(scheme + "://" + tetcd)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if !strings.HasPrefix(v, probetests[0].version) || s != probetests[0].secure {
-		t.Errorf("discovery.ProbeEtcd(%s) => (%s, %t) want (%s.x.x, %t)", tetcd, v, s, probetests[0].version, probetests[0].secure)
-	}
-	_ = util.Etcddown()
-	// etcd3 discovery:
-	p = probetests[1].port
-	tetcd = "localhost:" + p
-	err = util.Etcd3up(p)
-	if err != nil {
-		t.Errorf("Can't launch etcd at %s: %s", tetcd, err)
-		return
-	}
-	v, s, err = ProbeEtcd(probetests[1].scheme + "://" + tetcd)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	if !strings.HasPrefix(v, probetests[1].version) || s != probetests[1].secure {
-		t.Errorf("discovery.ProbeEtcd(%s) => (%s, %t) want (%s.x.x, %t)", tetcd, v, s, probetests[1].version, probetests[1].secure)
+	if !strings.HasPrefix(v, version) || s != secure {
+		t.Errorf("discovery.ProbeEtcd(%s) => (%s, %t) want (%s.x.x, %t)", tetcd, v, s, version, secure)
 	}
 }
